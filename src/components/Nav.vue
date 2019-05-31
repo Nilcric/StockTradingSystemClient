4fe29b4a328b1d6a47e82a86b0a5a021415923cb
Circle Lin <nilcric@outlook.com>
2019-05-31 09:40:27 +0800

    First night

---
+++ src/components/Nav.vue
@@ -0,0 +1,50 @@
+<template>
+  <el-menu default-active="2" class="el-menu-vertical-demo" :router="true">
+    <el-menu-item index="/">
+      <i class="el-icon-menu"/>
+      <span slot="title">Hello World 页（测试用）</span>
+    </el-menu-item>
+    <el-menu-item index="/Home">
+      <i class="el-icon-s-home"/>
+      <span slot="title">主页</span>
+    </el-menu-item>
+    <el-menu-item index="/Stock">
+      <i class="el-icon-s-marketing"/>
+      <span slot="title">持有股票</span>
+    </el-menu-item>
+    <el-menu-item index="/Finance">
+      <i class="el-icon-s-finance"/>
+      <span slot="title">资金账户</span>
+    </el-menu-item>
+    <el-menu-item index="Buy">
+      <i class="el-icon-shopping-cart-2"/>
+      <span slot="title">购买股票</span>
+    </el-menu-item>
+    <el-menu-item index="Sell">
+      <i class="el-icon-sold-out"/>
+      <span slot="title">出售股票</span>
+    </el-menu-item>
+    <el-menu-item index="Command">
+      <i class="el-icon-sold-out"/>
+      <span slot="title">撤销指令</span>
+    </el-menu-item>
+    <el-menu-item index="Search">
+      <i class="el-icon-search"/>
+      <span slot="title">查询股票</span>
+    </el-menu-item>
+    <el-menu-item index="Alert">
+      <i class="el-icon-bell"/>
+      <span slot="title">设置提醒</span>
+    </el-menu-item>
+    <el-menu-item index="Setting">
+      <i class="el-icon-key"/>
+      <span slot="title">修改密码</span>
+    </el-menu-item>
+  </el-menu>
+</template>
+
+<script>
+export default {
+  name: "Nav"
+};
+</script>
---
+++ src/views/Stock.vue
@@ -0,0 +1,83 @@
+<template>
+  <div>
+    <h1>持有股票</h1>
+    <el-table :data="tableData">
+      <el-table-column prop="id" label="股票代码"/>
+      <el-table-column prop="name" label="股票名称"/>
+      <el-table-column prop="amount" label="持有数量"/>
+      <el-table-column prop="price" label="当前价格"/>
+      <el-table-column prop="cost" label="持有成本"/>
+      <el-table-column prop="profit" label="收益">
+        <template
+          slot-scope="scope"
+        >{{((scope.row.price - scope.row.cost) * scope.row.amount).toFixed(2)}}</template>
+      </el-table-column>
+      <el-table-column label="操作" min-width="150">
+        <template slot-scope="scope">
+          <el-button size="mini">购买</el-button>
+          <el-button size="mini">出售</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Stock",
+  data() {
+    return {
+      tableData: [
+        {
+          id: "300059",
+          name: "东方财富",
+          amount: 140.3,
+          price: 12.32,
+          cost: 12.81,
+          profit: 13.92
+        },
+        {
+          id: "000066",
+          name: "中国长城",
+          amount: 107.1,
+          price: 10.23,
+          cost: 9.74,
+          profit: 21.42
+        },
+        {
+          id: "002161",
+          name: "远望谷",
+          amount: 93.7,
+          price: 7.92,
+          cost: 8.28,
+          profit: -8.03
+        },
+        {
+          id: "002195",
+          name: "二三四五",
+          amount: 84.98,
+          price: 5.1,
+          cost: 4.5,
+          profit: 13.92
+        },
+        {
+          id: "002131",
+          name: "利欧股份",
+          amount: 70.04,
+          price: 2.04,
+          cost: 2.1,
+          profit: 21.42
+        },
+        {
+          id: "002177",
+          name: "御银股份",
+          amount: 65.23,
+          price: 6.23,
+          cost: 5.1,
+          profit: -8.03
+        }
+      ]
+    };
+  }
+};
+</script>
